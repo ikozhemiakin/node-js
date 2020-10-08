87e3fdc4e6f996d48d758f15728ba24814fe2854
Ivan Kozhemiakin <ikozhemiakin@gomage.com>
2020-10-08 17:39:43 +0300

    Initialize search

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
+'use strict';
 const express = require("express");
 const exphbs = require('express-handlebars');
+const hbs1 = require("hbs");
 const mysql = require('mysql');
 const bodyParser = require("body-parser");
-
+const search = require("./search");
 const urlencodedParser = bodyParser.urlencoded({extended: false});
-
 
 const PORT = process.env.PORT || 3000
 
 let app = express();
+
 const hbs = exphbs.create({
     defaultLayout: 'index',
     extname: 'hbs'
   })
+let search1 = new search();
+
+hbs1.registerHelper("setNoResults", function () {
+    console.log(result);
+    console.log('No Results');
+    if (result.length == 0) {
+        return '<table width="90%" border="0" align="center"><tr><td valign="top">' +
+            '<font face="arial"><b><dl>' +
+            '<hr noshade width="100%">' +
+            '" returned no results.<hr noshade="" width="100%>' +
+            '</td></tr></table>';
+    }
+});
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
- 
 
 let connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'root',
   database : 'js'
 });
 
@@ -30,24 +44,21 @@ app.get('/', function (req, res) {
     res.render('form');
 });
 
-
-
 app.post('/result', urlencodedParser, function (req, res) {
 
     if(!req.body) return res.sendStatus(400);
 
-    const id = req.body.id;
+    global.entry = req.body.pageName;
+    search1.validate(entry);
 
-    connection.query('SELECT * FROM profiles WHERE id='+id, function (error, data) {
+
+    connection.query('SELECT * FROM profiles WHERE pageName="'+entry+'"', function (error, data) {
         if (error) throw error;
         res.render("result", {
-            users: data
+            pages: data
         });
     });
 });
-
-
-
 
 app.listen(PORT, () => {
     console.log('Сервер запущен...')
