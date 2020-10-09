46cc502f3dd38ed1e28a1582c728839f4fbdf6d6
Ivan Kozhemiakin <ikozhemiakin@gomage.com>
2020-10-09 11:43:46 +0300

    added no-result and url-html results pages

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars');
 // const hbs1 = require("hbs");
 const mysql = require('mysql');
 const bodyParser = require("body-parser");
-//const search = require("./search");
+const search = require("./search");
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 const PORT = process.env.PORT || 3000
@@ -14,9 +14,11 @@ let app = express();
 const hbs = exphbs.create({
     defaultLayout: 'index',
     extname: 'hbs'
-  })
-
-// let search1 = new search();
+})
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+global.search1 = new search();
 
 // hbs1.registerHelper("setNoResults", function () {
 //     console.log(result);
@@ -35,49 +37,39 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 let connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'js'
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'js'
 });
 
 app.get('/', function (req, res) {
     res.render('form');
 });
 
-function validate(data){
-    if (data != 'error'){
-        return true;
-    } else{
-        return false;
-    }
-}
 
 app.post('/result', urlencodedParser, function (req, res) {
 
-    if(!req.body) return res.sendStatus(400);
+    if (!req.body) return res.sendStatus(400);
 
-    let entry = req.body.pageName;
-    // search1.validate(entry);
-
-    if (validate(entry)) {
-        connection.query('SELECT * FROM profiles WHERE pageName="'+entry+'"', function (error, data) {
-            if (error) throw error;
-
-            if (data.length != 0) {
-                res.render("success", {
-                    pages: data
+    var entry = req.body.pageName;
+    var reentry = search1.validate(entry);
+    connection.query('SELECT * FROM profiles WHERE pageName="' + entry + '"', function (error, data) {
+        if (error) throw error;
+        if ((data.length != 0) && (entry != "url: html")) {
+            res.render("result", {
+                pages: data
+            });
+        } else if ((data.length == 0) && (entry != "url: html")) {
+            res.render("no-results");
+        } else if (entry == "url: html") {
+            connection.query('SELECT * FROM profiles', function (error, data) {
+                res.render("url-html", {
+                    urls: data
                 });
-            } else {
-                res.render("no-results");
-            }
-
-        });
-    } else {
-        res.render("error");
-    }
-
-
+            });
+        }
+    });
 });
 
 app.listen(PORT, () => {
--- search.js
+++ search.js
@@ -10,6 +10,7 @@ var showMatches = 10;
 var copyArray = [];
 
 Search.prototype.validate = function (entry) {
+    global.entry = entry;
     if (entry.charAt(0) == '+') {
         entry = entry.substring(1, entry.length);
         searchType = SEARCHURL;
@@ -31,83 +32,69 @@ Search.prototype.validate = function (entry) {
         document.forms[0].query.focus();
         return;
     }
-    this.convertString(entry);
+    // this.convertString(entry);
+    return entry;
 };
 
-Search.prototype.convertString = function (reentry) {
-    var searchArray = reentry.split(" ");
-    if (searchType = (SEARCHANY | SEARCHALL)) {
-        this.requireAll(searchArray);
-    } else {
-        this.allowAny(searchArray);
-    }
-}
+// Search.prototype.convertString = function (reentry) {
+//     var searchArray = reentry.split(" ");
+//     if (searchType = (SEARCHANY | SEARCHALL)) {
+//         console.log(searchArray);
+//         // this.requireAll(searchArray, dbData);
+//     } else {
+//         this.allowAny(searchArray);
+//     }
+// }
 
-Search.prototype.allowAny = function (t) {
-    var findings = [0];
-    for (var i = 0; i < entry.length; i++) {
-        var compareElement = entry[i].toUpperCase();
-        if (searchType == SEARCHANY) {
-            var refineElement = compareElement.substring(0, compareElement.indexOf('|HTTP'))
-        } else {
-            var refineElement = compareElement.substring(compareElement.indexOf('|HTTP'), compareElement.length);
-        }
-        for (var j = 0; j < t.length; j++) {
-            var compareString = t[j].toUpperCase();
-            if (refineElement.indexOf(compareString) != -1) {
-                findings[findings.length] = entry[i];
-                break;
-            }
-        }
-    }
-    this.verifyManage(findings);
-}
-
-Search.prototype.requireAll = function (t) {
-    var findings = [];
-    for (var i = 0; i < entry.length; i++) {
-        var allConfirmation = true;
-        var allString = entry[i].toUpperCase();
-        var refineAllString = allString.substring(0, allString.indexOf('|HTTP'));
-        for (var j = 0; j < t.length; j++) {
-            var allElement = t[j].toUpperCase();
-            if (refineAllString.indexOf(allElement) == -1) {
-                allConfirmation = false;
-                continue;
-            }
-        }
-        if (allConfirmation) {
-            findings[findings.length] = entry[i];
-        }
-    }
-    global.result = findings;
-    this.verifyManage(findings);
-}
-
-Search.prototype.verifyManage = function (resultSet) {
-    if (resultSet.length == 0) {
-        console.log('No results');
-    } else {
-        copyArray = resultSet.sort();
-        // this.formatResults(copyArray, currentMatch, showMatches);
-    }
-}
-
-Search.prototype.noMatch = function () {
-    // if ('undefined' != typeof screen) {
-    //     // your code here
-    //
-    // var win = docObj.open();
-    // docObj.innerHTML +=
-    // document.getElementById('frame1').innerHTML ='<html><head><title>Search Results></title></head>' +
-    //     '<body bgcolor="white" text="black">' +
-    //     '<table width="90%" border="0" align="center"><tr><td valign="top">' +
-    //     '<font face="arial"><b><dl>' +
-    //     '<hr noshade width="100%">'+
-    //     '" returned no results.<hr noshade="" width="100%>' +
-    //     '</td></tr></table></body></html>';
-    // }
-    // docObj.writeln();
-    // docObj.close();
-    // document.forms[0].query.select();
-}
+// Search.prototype.allowAny = function (t) {
+//     console.log('aaaaa');
+//     var findings = [0];
+//     for (var i = 0; i < entry.length; i++) {
+//         var compareElement = entry[i].toUpperCase();
+//         if (searchType == SEARCHANY) {
+//             var refineElement = compareElement.substring(0, compareElement.indexOf('|HTTP'))
+//         } else {
+//             var refineElement = compareElement.substring(compareElement.indexOf('|HTTP'), compareElement.length);
+//         }
+//         for (var j = 0; j < t.length; j++) {
+//             var compareString = t[j].toUpperCase();
+//             if (refineElement.indexOf(compareString) != -1) {
+//                 findings[findings.length] = entry[i];
+//                 break;
+//             }
+//         }
+//     }
+//     this.verifyManage(findings);
+// }
+//
+// Search.prototype.requireAll = function (t, profiles) {
+//     var findings = [];
+//     for (var i = 0; i < profiles.length; i++) {
+//         var allConfirmation = true;
+//         var allString = [profiles[i].pageName.toUpperCase() + '|' + profiles[i].pageDescription.toUpperCase() + '|' + profiles[i].url.toUpperCase()];
+//         var refineAllString = allString.substring(0, allString.indexOf('|HTTP'));
+//         for (var j = 0; j < t.length; j++) {
+//             var allElement = t[j].toUpperCase();
+//             if (refineAllString.indexOf(allElement) == -1) {
+//                 allConfirmation = false;
+//                 continue;
+//             }
+//         }
+//         if (allConfirmation) {
+//             findings[findings.length] = profiles[i];
+//         }
+//     }
+//     console.log(findings);
+//     this.verifyManage(findings);
+// }
+//
+// Search.prototype.verifyManage = function (resultSet) {
+//     if (resultSet.length == 0) {
+//         console.log('No results');
+//     } else {
+//         console.log('kkkk');
+//         // copyArray = resultSet.sort();
+//         // console.log(copyArray);
+//         // return copyArray;
+//     }
+// }
