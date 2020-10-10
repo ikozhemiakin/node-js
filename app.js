367d8ec97c44f0d68c9cdc0d68579c63b2018e7e
Ivan Kozhemiakin <ikozhemiakin@gomage.com>
2020-10-10 16:01:03 +0300

    Changed part of code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const exphbs = require('express-handlebars');
 const mysql = require('mysql');
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
@@ -18,17 +19,6 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-// hbs1.registerHelper("setNoResults", function () {
-//     console.log(result);
-//     console.log('No Results');
-//     if (result.length == 0) {
-//         return '<table width="90%" border="0" align="center"><tr><td valign="top">' +
-//             '<font face="arial"><b><dl>' +
-//             '<hr noshade width="100%">' +
-//             '" returned no results.<hr noshade="" width="100%>' +
-//             '</td></tr></table>';
-//     }
-// });
 
 let connection = mysql.createConnection({
     host: 'localhost',
@@ -36,33 +26,23 @@ let connection = mysql.createConnection({
     password: 'root',
     database: 'js'
 });
+app.use('/', require('./routes/myRouter'))
 
 app.get('/', function (req, res) {
     res.render('form');
 });
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-app.post('/result', urlencodedParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    var entry = req.body.pageName;
+app.use(bodyParser.json());
 
-
-    connection.query('SELECT * FROM profiles WHERE pageName="' + entry + '"', function (error, data) {
-
-        if (error) throw error;
-        if ((data.length == 0) && (entry != "url: html")) {
-            res.render("no-results");
-        } else if (entry == "url: html") {
-            connection.query('SELECT * FROM profiles', function (error, data) {
-                res.render("url-html", {
-                    urls: data
-                });
-            });
-        }
-    });
-});
-app.get('/result', urlencodedParser, function (req, res) {
-    var page = req.body.page;
+app.route('/result').get(function (req, res) {
+    console.log("Не работает");
+    res.send('GET request to the homepage');
+    var page = req.query.page;
+    console.log(page);
     var numPerPage = 10;
     var skip = (page - 1) * numPerPage;
     var limit = skip + ',' + numPerPage; // Here we compute the LIMIT parameter for MySQL query
@@ -85,6 +65,21 @@ app.get('/result', urlencodedParser, function (req, res) {
                         pages: rows
                     });
                 }
+            });
+        }
+    });
+}).post(function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    var entry = req.body.pageName;
+    connection.query('SELECT * FROM profiles WHERE pageName="' + entry + '"', function (error, data) {
+        if (error) throw error;
+        if ((data.length == 0) && (entry != "url: html")) {
+            res.render("no-results");
+        } else if (entry == "url: html") {
+            connection.query('SELECT * FROM profiles', function (error, data) {
+                res.render("url-html", {
+                    urls: data
+                });
             });
         }
     });
