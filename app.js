0bc3d42bb3bd5445ae992a3af39aeb79a1cce770
Ivan Kozhemiakin <ikozhemiakin@gomage.com>
2020-10-11 10:16:28 +0300

    Added pagination via express-pagination module

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-'use strict';
 const express = require("express");
 const exphbs = require('express-handlebars');
+var paginateHelper = require('express-handlebars-paginate');
 const mysql = require('mysql');
 const bodyParser = require("body-parser");
-const path = require('path');
+// const path = require('path');
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
@@ -15,6 +15,8 @@ const hbs = exphbs.create({
     defaultLayout: 'index',
     extname: 'hbs'
 })
+hbs.handlebars.registerHelper('paginateHelper', paginateHelper.createPagination);
+
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
@@ -26,63 +28,40 @@ let connection = mysql.createConnection({
     password: 'root',
     database: 'js'
 });
-app.use('/', require('./routes/myRouter'))
-
-app.get('/', function (req, res) {
-    res.render('form');
-});
-
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
 app.use(bodyParser.json());
 
-app.route('/result').get(function (req, res) {
-    console.log("Не работает");
-    res.send('GET request to the homepage');
+app.get('/', function (req, res) {
+    res.render('form');
+});
+
+app.get('/result', function (req, res) {
+    if (!req.body) return res.sendStatus(400);
     var page = req.query.page;
-    console.log(page);
+    var entry = req.query.pageName;
     var numPerPage = 10;
     var skip = (page - 1) * numPerPage;
-    var limit = skip + ',' + numPerPage; // Here we compute the LIMIT parameter for MySQL query
-    connection.query('SELECT count(*) as numRows FROM profiles', function (err, rows, fields) {
-        if (err) {
-            console.log("error: ", err);
-        } else {
-            var numRows = rows[0].numRows;
-            var numPages = Math.ceil(numRows / numPerPage);
-            connection.query('SELECT * FROM profiles LIMIT ' + limit, function (err, rows, fields) {
-                if (err) {
-                    console.log("error: ", err);
-                } else {
+    var limit = skip + ',' + numPerPage;
+    if (entry != 'endefiend') {
+        console.log('SELECT * FROM profiles WHERE pageName="' + entry + '" LIMIT ' + limit);
+        connection.query('SELECT * FROM profiles WHERE pageName="' + entry + '" LIMIT ' + limit, function (error, data) {
+            if (error) throw error;
+            if ((data.length == 0) && (entry != "url: html")) {
+                res.render("no-results");
+            } else if (entry == "url: html") {
+                // res.render('url-html', {urls: data, pagination: { page: 1, limit:10,totalRows: 11, queryParams: data }});
+                // res.render("url-html", {
+                //     urls: data
+                // });
+            } else {
+                res.render('result', {pages: data, pagination: {page: 2, limit: 10, totalRows: 11}});
 
-                    console.log(rows)
-                    // result(null, rows,numPages);
-                    res.render("result", {
-                        firstPage: 1,
-                        numPages: numPages,
-                        pages: rows
-                    });
-                }
-            });
-        }
-    });
-}).post(function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    var entry = req.body.pageName;
-    connection.query('SELECT * FROM profiles WHERE pageName="' + entry + '"', function (error, data) {
-        if (error) throw error;
-        if ((data.length == 0) && (entry != "url: html")) {
-            res.render("no-results");
-        } else if (entry == "url: html") {
-            connection.query('SELECT * FROM profiles', function (error, data) {
-                res.render("url-html", {
-                    urls: data
-                });
-            });
-        }
-    });
+            }
+        });
+    }
 });
 
 app.listen(PORT, () => {
