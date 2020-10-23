fed1c16410209eee3745fcb76a483eda32a03993
Ivan Kozhemiakin <ikozhemiakin@gomage.com>
2020-10-23 23:22:01 +0300

    added axios connection from express to react

--- api/app.js
+++ api/app.js
@@ -8,6 +8,8 @@ const exphbs = require('express-handlebars');
 var paginateHelper = require('express-handlebars-paginate');
 const mysql = require('mysql');
 const bodyParser = require("body-parser");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
@@ -27,12 +29,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
 hbs.handlebars.registerHelper('paginateHelper', paginateHelper.createPagination);
 
 app.use(bodyParser.urlencoded({extended: true}));
+
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -50,74 +50,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/testAPI", testAPIRouter);
+app.listen(PORT, () => console.log("Backend server live on " + PORT));
+
 app.get('/', function (req, res) {
     connection.query('SELECT * FROM categories', function (error, data) {
-        res.render('home', {categories: data});
+
+        res.send({ categories: data });
     });
 });
 
-app.get('/category', function (req, res) {
-    let page = req.query.page;
-    let categoryId = req.query.id;
-    if (!page) page = 1
-    let entry = req.query.pageName;
-    let numPerPage = 5;
-    let skip = (page - 1) * numPerPage;
-    let limit = skip + ',' + numPerPage;
-    let queryParams = {
-        'id': categoryId
-    }
-    // console.log('SELECT * FROM products LIMIT ' + limit + ');
-    connection.query('SELECT * FROM products where category_id=' + categoryId + ' LIMIT ' + limit + '; SELECT * FROM categories where id=' + categoryId + '; SELECT COUNT(1) FROM products where category_id=' + categoryId, function (error, data) {
-        let totalRows = data[2][0]['COUNT(1)'];
-        console.log(totalRows);
-        res.render('category', {
-            products: data[0],
-            categories: data[1],
-            pagination: {page: page, limit: numPerPage, totalRows: totalRows, queryParams: queryParams}
-        });
 
-    });
-
-});
-
-app.get('/result', function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    let page = req.query.page;
-    if (!page) page = 1
-    let entry = req.query.pageName;
-    let numPerPage = 5;
-    let skip = (page - 1) * numPerPage;
-    let limit = skip + ',' + numPerPage;
-    let queryParams = {
-        'pageName': entry
-    }
-    console.log(entry);
-    connection.query('SELECT * FROM profiles WHERE pageName="' + entry + '" LIMIT ' + limit + '; SELECT COUNT(1) FROM profiles', function (error, data) {
-
-        let totalRows = data[1][0]['COUNT(1)'];
-        if (error) throw error;
-        if ((data[0].length == 0) && (entry != "url: html")) {
-            res.render("no-results");
-        } else if (entry == "url: html") {
-            connection.query('SELECT * FROM profiles LIMIT ' + limit + '; SELECT COUNT(1) FROM profiles', function (error, data) {
-                res.render('url-html', {
-                    urls: data[0],
-                    pagination: {page: page, limit: numPerPage, totalRows: totalRows, queryParams: queryParams}
-                });
-            });
-        } else {
-            res.render('result', {
-                pages: data[0],
-                pagination: {page: page, limit: numPerPage, totalRows: totalRows, queryParams: queryParams}
-            });
-        }
-    });
-});
-
-app.listen(PORT, () => {
-    console.log('Сервер запущен...')
-})
+// app.listen(PORT, () => {
+//     console.log('Сервер запущен...')
+// })
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,35 +1,86 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+// import Category from "./Category";
+import Axios from "axios";
+// import Category from "./Category";
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { apiResponse: "" };
+function App() {
+    Axios({
+        method: "GET",
+        url: "http://localhost:9000/",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => {
+        console.log(res.data.message);
+    });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+
+    // async componentDidMount() {
+    //
+    //     Axios({
+    //         method: "GET",
+    //         url: "http://localhost:3000/",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         }
+    //     }).then(res => {
+    //         this.setState({name: res.data.name, description: res.data.description});
+    //
+    //         console.log(res.data.message);
+    //     });
+    //
+    // }
+    //
+    //     const { isLoading, name, description } = this.state;
+    //     return (
+    //         <div className="App">
+    //             <header className="App-header">
+    //                 <img src={logo} className="App-logo" alt="logo" />
+    //                 <h1 className="App-title">Welcome to React</h1>
+    //             </header>
+    //             <p className="App-intro">
+    //             </p>
+    //             <Category isLoading={isLoading} name={name} description={description} />
+    //         </div>
+    //     );
+        // return (
+        //     <div className="App">
+        //         <header className="App-header">
+        //             <img src={logo} className="App-logo" alt="logo" />
+        //             <h1 className="App-title">Welcome to React</h1>
+        //         </header>
+        //         <ul>
+        //             <Category name="Jessica Doe" description="Description" />
+        //             {this.state.apiResponse.map(el => (
+        //                 <li>
+        //                     {el.name}
+        //                 </li>
+        //             ))}
+        //         </ul>
+        //     </div>
+        // );
     }
 
-    callAPI() {
-        fetch("http://localhost:9001/testAPI")
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
-            .catch(err => err);
-    }
-
-    componentDidMount() {
-        this.callAPI();
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
-            </div>
-        );
-    }
-}
 
 export default App;
+
